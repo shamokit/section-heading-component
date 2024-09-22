6449b6713daf37422b2c9f4bd853d5f101a4846e
shamokit <contact@shamokit.com>
2024-09-23 00:38:39 +0900

    ♻️ refactor

--- src/lib/Heading.svelte
+++ src/lib/Heading.svelte
@@ -1,12 +1,6 @@
-<script context="module" lang="ts">
-	import { getContext, setContext, type ComponentProps } from 'svelte';
-
-	const KEY = Symbol('heading');
-	export const setHeadingContext = (v: number) => setContext(KEY, v);
-	export const getHeadingContext = () => getContext<number>(KEY);
-</script>
-
 <script lang="ts">
+	import { getHeadingContext } from './Section.svelte';
+
 	let level = getHeadingContext();
 	$: tag = level ? `h${level}` : 'h2';
 </script>
--- src/lib/Section.svelte
+++ src/lib/Section.svelte
@@ -1,6 +1,12 @@
-<script>
-	import { getHeadingContext, setHeadingContext } from './Heading.svelte';
+<script context="module" lang="ts">
+	import { getContext, setContext } from 'svelte';
 
+	const KEY = Symbol('heading');
+	const setHeadingContext = (v: number) => setContext(KEY, v);
+	export const getHeadingContext = () => getContext<number>(KEY);
+</script>
+
+<script>
 	let level = getHeadingContext();
 	$: setHeadingContext(level ? level + 1 : 2);
 </script>
